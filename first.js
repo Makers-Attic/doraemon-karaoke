9128a6f535d060e247e555b0b7d50902e7c3b1fd
dplayerd <dplayerd@gmail.com>
2016-02-03 23:27:18 +0800

    初版
    剛建立伺服器和專案檔

---
+++ first.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+
+var shareData = {
+    SleepMode: false,
+    PeopleClosed: false,
+    Scolling: false,
+    MovieUrl: ""
+};
+
+http.createServer(function(req, res) {
+    var responseText = 
+        JSON.stringify(shareData);
+    
+    
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(responseText);    
+}).listen(port);
+
